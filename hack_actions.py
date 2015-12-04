b7515cb570ad56653c4bf5b11d015add896648ad
Bharadwaj Machiraju <tunnelshade@gmail.com>
2015-12-04 17:29:02 +0530

    Base added for feature trails

--- hack_actions.py
+++ hack_actions.py
@@ -3,7 +3,6 @@ import numpy as np
 class HackAction(object):
     dependent_dims = []
     dependency_dims = []
-    str_repr = 'HackAction'
 
     def __str__(self):
         return(str_repr)
@@ -12,74 +11,70 @@ class HackAction(object):
         return(str_repr)
 
 
-class WhatwebAction(HackAction):
-    str_repr = 'whatweb'
+class Whatweb(HackAction):
 
     def run(self, url):
         cms = 0
         if url == 'http://typographica.org':
-            cms = 1
+            cms = 'WordPress'
         elif url == 'https://www.drupal.org':
-            cms = 2
+            cms = 'Drupal'
         elif url == 'http://www.mb-photography.com':
-            cms = 3
-        return(np.array([[0, cms]]))
+            cms = 'Joomla'
+        return({'cms': cms})
         f = os.path.join("/tmp", str(random.randint(1000, 9999)))
         p = subprocess.Popen(["whatweb", "-q", "--log-json", str(f), url])
         r = p.wait()
         data = json.load(open(f, 'r'))
         if "WordPress" in data["plugins"]:
-            cms = 1
+            cms = 'WordPress'
         elif "Drupal" in data["plugins"]:
-            cms = 2
+            cms = 'Drupal'
         elif "Joomla" in data["plugins"]:
-            cms = 3
+            cms = 'Joomla'
         os.remove(f)
-        return(np.array([[0, cms]]))
+        return({'cms': cms})
 
 
-class WpscanAction(HackAction):
-    dependent_dims = [0]
-    str_repr = 'wpscan'
+class Wpscan(HackAction):
+    dependent_dims = ['cms']
 
     def run(self, url):
         version = 0
         if url == 'http://typographica.org':
             # print("Wordpress version identified")
-            version = 1
-        return(np.array([[1, version]]))
+            version = 'Detected'
+        return({'cms_version': version})
         try:
             data = subprocess.check_output(["wpscan", "--no-color", "--url", url])
         except subprocess.CalledProcessError, e:
             data = e.output
-        version = 0 if re.search("WordPress version can not be detected", data) else 1
-        return(np.array([[1, version]]))
+        version = 0 if re.search("WordPress version can not be detected", data) else 'Detected'
+        return({'cms_version': version})
 
-class JoomscanAction(HackAction):
-    dependent_dims = [0]
-    str_repr = 'joomscan'
+class Joomscan(HackAction):
+    dependent_dims = ['cms']
 
     def run(self, url):
         version = 0
         if url == 'http://www.mb-photography.com':
             # print("Joomla version identified")
-            version = 1
-        return(np.array([[1, version]]))
+            version = 'Detected'
+        return({'cms_version': version})
         data = subprocess.check_output(["joomscan", "-pe", "-nf", "-u", url])
-        version = 0 if re.search("Is it sure a Joomla\?", data) else 1
-        return(np.array([[1, version]]))
+        version = 0 if re.search("Is it sure a Joomla\?", data) else 'Detected'
+        return({'cms_version': version})
 
 
-class DroopescanAction(HackAction):
-    dependent_dims = [0]
-    str_repr = 'droopescan'
+class Droopescan(HackAction):
+    dependent_dims = ['cms']
 
     def run(self, url):
         version = 0
         if url == 'https://www.drupal.org':
             # print("Drupal version identified")
-            version = 1
-        return(np.array([[1, version]]))
+            version = 'Detected'
+        return({'cms_version': version})
         data = subprocess.check_output(["droopescan", "scan", "drupal", "-e", "v", "-u", url])
-        version = 0 if re.search("No version found", data) else 1
-        return(np.array([[1, version]]))
+        version = 0 if re.search("No version found", data) else 'Detected'
+        return({'cms_version': version})
--- hack_domain.py
+++ hack_domain.py
@@ -1,7 +1,11 @@
 from rlpy.Domains.Domain import Domain
-from hack_actions import *
 import numpy as np
+import os
+import json
 import random
+import importlib
+import hack_actions
+
 
 class HackDomain(Domain):
     #: Reward for each timestep spent in the goal region
@@ -14,43 +18,117 @@ class HackDomain(Domain):
     def __init__(self):
         self.url = None
         self.start = 0
-        self.statespace_limits = np.array([[0,3], [0, 1]])
+        self.datastore = Datastore()
         self.continuous_dims = []
-        self.DimNames = ['cms', 'version']
-        self.actions_num = 4
+        self.DimNames = self.datastore.ordered_dim_names
+        self.actions = [cls() for cls in hack_actions.HackAction.__subclasses__()]
+        self.actions_num = len(self.actions)
+        self.statespace_limits = np.array([[0,5] for i in range(len(self.datastore.ordered_dim_names))])
         self.discount_factor = 0.9
-        self.actions = [WhatwebAction(), WpscanAction(), DroopescanAction(), JoomscanAction()]
         super(HackDomain, self).__init__()
         self.s0()
 
     def s0(self):
-        self.url = random.choice(['https://www.drupal.org', 'http://typographica.org', 'http://www.mb-photography.com'])
-        self.state = np.array([0 for dummy in xrange(0, self.state_space_dims)])
-        return(self.state, self.isTerminal(), self.possibleActions())
+        state = self.datastore.get_state(new=True)
+        self.url = self.datastore.current_target
+        return(state, self.isTerminal(), self.possibleActions())
 
     def possibleActions(self):
         pa = []
+        zero_state_features = []
+        state = self.datastore.get_state()
+        for j, v in enumerate(state):
+            if v == 0:
+                zero_state_features.append(self.DimNames[j])
         for j, a in enumerate(self.actions):
-            if 0 not in [self.state[i] for i in a.dependent_dims]:
+            if len(list(set(a.dependent_dims) & set(zero_state_features))) == 0:
                 pa.append(j)
         return(pa)
 
     def isTerminal(self):
-        s = self.state
-        return(s[-1] != 0)
+        s = self.datastore.get('cms_version')
+        return(s != 0)
 
     def step(self, a):
         result = self.actions[a].run(self.url)
-        for i in result:
-            self.state[i[0]] = i[1]
+        for k, v in result.items():
+            self.datastore.set(k, v)
         t = self.isTerminal()
         r = self.GOAL_REWARD if t else self.STEP_REWARD
-        # print("Next State %s (Terminal: %r)" % (str(self.state), t))
-        return(r, self.state.copy(), t, self.possibleActions())
+        self.datastore.save()
+        return(r, self.datastore.get_state(), t, self.possibleActions())
 
     def showLearning(self, representation):
         terminal = self.isTerminal()
         actions = self.possibleActions()
-        ba = representation.bestActions(self.state, terminal, actions)
-        print("State: %s Best Action: %s" % (str(self.state), str(ba)))
+        state = self.datastore.get_state()
+        ba = representation.bestActions(state, terminal, actions)
+        if not terminal:
+            # print("Possible Actions: %s " % (str(actions)))
+            print("State: %s Best Action: %s" % (str(state), type(self.actions[ba[0]]).__name__))
         return
+
+
+target_dict = {
+    "cms": 0,
+    "cms_version": 0,
+    "port": 0,
+    "protocol": 0
+}
+
+class Datastore(object):
+    def __init__(self, f='data.json'):
+        self.f = f
+        self.current_target = None
+        if os.path.exists(self.f):
+            with open(self.f, 'r') as fp:
+                self.data = json.load(fp)
+        else:
+            self.data = {"targets":{}}
+        self.ordered_dim_names = target_dict.keys()
+        self.ordered_dim_names.sort()
+        self.target_urls = ['https://www.drupal.org', 'http://typographica.org', 'http://www.mb-photography.com']
+
+    def save(self):
+        with open(self.f, 'w') as fp:
+            json.dump(self.data, fp)
+
+    def _get_prop_numbered_value(self, prop_name, prop_value):
+        key = prop_name + "___set"
+        try:
+            dataset = self.data[key]
+        except KeyError:
+            self.data[key] = [0]
+        finally:
+            try:
+                num_value = self.data[key].index(prop_value)
+            except ValueError:
+                self.data[key].append(prop_value)
+                num_value = len(self.data[key]) - 1
+            finally:
+                return(num_value)
+
+    def reset(self, target_url):
+        self.data["targets"][target_url] = dict(target_dict)
+
+    def get(self, prop_name, target=None):
+        target = target if target else self.current_target
+        return(self.data["targets"][target][prop_name])
+
+    def set(self, prop_name, prop_value, target=None):
+        target = target if target else self.current_target
+        try:
+            targets = self.data["targets"][target]
+        except KeyError:
+            self.data["targets"][target] = dict(target_dict)
+        finally:
+            self.data["targets"][target][prop_name] = self._get_prop_numbered_value(prop_name, prop_value)
+
+    def get_state(self, new=False):
+        if new == True:
+            self.current_target = random.choice(self.target_urls)
+            self.reset(self.current_target)
+            state = [0 for x in self.ordered_dim_names]
+        else:
+            state = [self.data["targets"][self.current_target][x] for x in self.ordered_dim_names]
+        return(state)
