034ea6adb8d04cbad1f6fe907119e2f845bf8ba9
Bharadwaj Machiraju <tunnelshade@gmail.com>
2016-03-08 17:38:12 +0530

    Transfer commit

--- hack_actions.py
+++ hack_actions.py
@@ -7,66 +7,21 @@ import numpy as np
 
 from urlparse import urlparse
 
+ACTIONS = []
+
 class HackAction(object):
     dependent_dims = []
     dependency_dims = []
-    aggression = 0
+    def __init__(self, s):
+        self.string = s
 
     def hash_string(self, s):
         m = md5.new()
         m.update(s)
         return(m.hexdigest())
 
-    def run_command(self, c_list, f=None):
-        """
-        If f is provided then the file content is cached and returned everytime
-        """
-        content = ''
-        if not os.path.exists('command_cache'):
-            os.makedirs('command_cache')
-        path = os.path.join('command_cache', self.hash_string(' '.join(c_list)))
-        if os.path.exists(path):
-            content = open(path, 'r').read()
-        else:
-            try:
-                print("Running %s" % (' '.join(c_list)))
-                content = subprocess.check_output(c_list)
-            except subprocess.CalledProcessError, e:
-                print("Command %s exited with non-zero status" % (' '.join(c_list)))
-                content = e.output
-            if f != None:
-                content = open(f, 'r').read()
-                os.remove(f)
-            with open(path, 'w') as fp:
-                fp.write(content)
-        return(content)
-
-    def make_get_request(self, url):
-        content = ''
-        if not os.path.exists('request_cache'):
-            os.makedirs('request_cache')
-        path = os.path.join('request_cache', self.hash_string(url))
-        if os.path.exists(path):
-            content = open(path, 'r').read()
-        else:
-            resp = requests.get(url)
-            content = resp.text
-            with open(path, 'w') as fp:
-                fp.write(content)
-        return(content)
-
-    def parse_url(self, target_url):
-        parsed_url = urlparse(target_url)
-        t = {}
-        t['protocol'] = parsed_url.scheme
-        t['port'] = parsed_url.port
-        t['hostname'] = parsed_url.hostname
-        if t['port'] == None:
-            if parsed_url.scheme == 'http':
-                t['port'] = 80
-            elif parsed_url.scheme == 'https':
-                t['port'] = 443
-        return(t)
+    def run(self, payload_list):
+        return(self.string)
 
     def __str__(self):
         return(str_repr)
@@ -75,54 +30,38 @@ class HackAction(object):
         return(str_repr)
 
 
-class Whatweb(HackAction):
-    dependency_dims = ['cms']
-    aggression = 2
 
-    def run(self, url):
-        cms = 0
-        f = os.path.join("/tmp", self.hash_string(url))
-        data = self.run_command(["whatweb", "-q", "--log-json", str(f), url], f=f)
-        data = json.loads(data)
-        if "WordPress" in data["plugins"]:
-            cms = 'WordPress'
-        elif "Drupal" in data["plugins"]:
-            cms = 'Drupal'
-        elif "Joomla" in data["plugins"]:
-            cms = 'Joomla'
-        return({'cms': cms})
+class TagAction(HackAction):
+    """
+    Action representing a HTML tag
+    """
+    pass
+
+TAGS = ('a', 'abbr', 'acronym', 'address', 'applet', 'embed', 'object', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'colgroup', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'ul', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'input', 'legend', 'fieldset', 'li', 'link', 'main', 'map', 'mark', 'menu', 'menuitem')
+for i in TAGS:
+    ACTIONS.append('<' + TagAction(i) + '>')
 
 
-class Wpscan(HackAction):
-    dependent_dims = ['cms']
-    dependency_dims = ['cms_version']
-    aggression = 2
 
-    def run(self, url):
-        version = 0
-        data = self.run_command(["wpscan", "--no-color", "--batch", "--url", url])
-        version = 0 if re.search("WordPress version can not be detected", data) else 'Detected'
-        return({'cms_version': version})
-
-class Joomscan(HackAction):
-    dependent_dims = ['cms']
-    dependency_dims = ['cms_version']
-    aggression = 2
-
-    def run(self, url):
-        version = 0
-        data = self.run_command(["joomscan", "-pe", "-nf", "-u", url])
-        version = 0 if re.search("Is it sure a Joomla\?", data) else 'Detected'
-        return({'cms_version': version})
+class ControlAction(HackAction):
+    """
+    Action representing a control character
+    """
+    pass
 
 
-class Droopescan(HackAction):
-    dependent_dims = ['cms']
-    dependency_dims = ['cms_version']
-    aggression = 2
+CONTROL_CHARS = [' ', '"', "'", '(', ')', '*', '+', '-', ',', ';', '<', '>', '=', '[', ']', '{', '}', '`']
+for i in CONTROL_CHARS:
+    ACTIONS.append(ControlAction(i))
 
-    def run(self, url):
-        version = 0
-        data = self.run_command(["droopescan", "scan", "drupal", "-e", "v", "-u", url])
-        version = 0 if re.search("No version found", data) else 'Detected'
-        return({'cms_version': version})
+MASTER_COUNTER_CONTROL_CHARS = {
+    "'": "'",
+    '"': '"',
+}
+
+COUNTER_CONTROL_CHARS = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '`': '`'
+}
--- hack_domain.py
+++ hack_domain.py
@@ -1,38 +1,143 @@
 from rlpy.Domains.Domain import Domain
 from urlparse import urlparse
+from selenium import webdriver
 
 import numpy as np
 import os
+import re
 import json
 import random
 import importlib
 import hack_actions
 
 
+class CustomHTMLParser(HTMLParser):
+    def __init__(self):
+        self.stack = []
+        self.taint = 'alert()'
+        self.found_in_tag_attr_param = False
+        self.found_in_tag_attr_value = False
+        self.found_in_data = False
+        self.trace = ''
+
+    @property
+    def found(self):
+        return (self.found_in_tag_attr_param or self.found_in_tag_attr_value or self.found_in_data)
+
+    def handle_startag(self, tag, attrs):
+        if self.found == False:
+            if tag != 'div':
+                self.stack.append(tag)
+            for param, value in attrs:
+                if self.taint in param:
+                    self.found_in_tag_attr_param = True
+                    self.trace = param
+                elif self.taint in value:
+                    self.found_in_tag_attr_value = True
+                    self.trace = value
+
+    def handle_endtag(self, tag):
+        if self.found == False:
+            if self.stack[-1] == tag:
+                self.stack.pop()
+
+    def handle_data(self, data):
+        if self.found == False:
+            if self.taint in data:
+                self.trace = data
+                self.found_in_data = True
+
+    def handle_startendtag(self, tag, attrs):
+        if self.found == False:
+            self.handle_starttag(tag, attrs)
+            self.handle_endtag(tag)
+
+    def handle_comment(self, data):
+        if self.found == False:
+            if self.taint in data:
+                self.found_in_data = True
+
+    def get_control_chars(self):
+        c_chars = ''
+        if self.found_in_tag_attr_param:
+            c_chars = c_chars + '>'
+        elif self.found_in_tag_attr_value:
+            c_chars = c_chars + '>'
+
+            c_chars = self._sink_environment[(self._sink_environment.index(self.trace) - 1)] + c_chars
+
+            # Tested  on "test(')', \"'\", {'taint':1}, \"(\", '\\'')"
+            # Following four loops will remove all the arguments except the ones with the trace
+            t = self.taint
+            s = self.trace
+            for m in re.findall(r"'(?:[^\\'\"]|(?:\\'))*'", s):
+                if t not in m:
+                    s = s.replace(m, '')
+            for m in re.findall(r'"(?:[^\\"\']|(?:\\"))*"', s):
+                if t not in m:
+                    s = s.replace(m, '')
+            if s.count('"') % 2 == 1:
+                for m in re.findall(r"'(?:[^\\']|(?:\\'))*'", s):
+                    if t not in m:
+                        s = s.replace(m, '')
+                for m in re.findall(r'"(?:[^\\"]|(?:\\"))*"', s):
+                    if t not in m:
+                        s = s.replace(m, '')
+            elif s.count("'") % 2 == 1:
+                for m in re.findall(r'"(?:[^\\"]|(?:\\"))*"', s):
+                    if t not in m:
+                        s = s.replace(m, '')
+                for m in re.findall(r"'(?:[^\\']|(?:\\'))*'", s):
+                    if t not in m:
+                        s = s.replace(m, '')
+
+            for c_plus, c_minus in hack_actions.COUNTER_CONTROL_CHARS.items():
+                for m in re.findall(r"\\%c[^\\%c]*\\%c" % (c_plus, c_minus, c_minus), s):
+                    if t not in m:
+                        s = s.replace(m, '')
+            temp_trace = s
+
+            temp_trace = temp_trace[temp_trace.index(self.taint)+len(self.taint):]
+
+            temp_control_chars = dict(hack_actions.COUNTER_CONTROL_CHARS)
+            temp_control_chars.update(hack_actions.MASTER_COUNTER_CONTROL_CHARS)
+            s = temp_trace
+            temp_trace = ''
+            for c in s:
+                if c in temp_control_chars.values():
+                    temp_trace += c
+            c_chars = temp_trace + c_chars
+            return(c_chars)
+
+
 class HackDomain(Domain):
     #: Reward for each timestep spent in the goal region
-    GOAL_REWARD = 5
+    GOAL_REWARD = 100
     #: Reward for each timestep
     STEP_REWARD = -1
     #: Set by the domain = min(100,rows*cols)
     episodeCap = 10
 
     def __init__(self):
-        self.url = None
         self.start = 0
         self.datastore = Datastore()
         self.continuous_dims = []
         self.DimNames = self.datastore.ordered_dim_names
-        self.actions = [cls() for cls in hack_actions.HackAction.__subclasses__()]
+        self.actions = hack_actions.ACTIONS
         self.actions_num = len(self.actions)
-        self.statespace_limits = np.array([[0,5] for i in range(len(self.datastore.ordered_dim_names))])
+        self.statespace_limits = np.array([[0,1000] for i in range(len(self.datastore.ordered_dim_names))])
         self.discount_factor = 0.9
         super(HackDomain, self).__init__()
         self.s0()
 
+        self.browser = webdriver.Firefox()
+        self._sink_environment = ''
+        self._payloads_environment = []
+
     def s0(self):
         state = self.datastore.get_state(new=True)
-        self.url = self.datastore.current_target
+        self._sink_environment = self.datastore.current_sink
+        self._payloads_environment = []
         return(state, self.isTerminal(), self.possibleActions())
 
     def possibleActions(self):
@@ -51,10 +156,25 @@ class HackDomain(Domain):
         s = self.datastore.get('cms_version')
         return(s != 0)
 
+    def _update_state(self):
+        e = self._sink_environment
+        parser = CustomHTMLParser()
+        parser.feed(e)
+        c_chars = parser.get_control_chars()
+        stack = parser.stack
+
+    def _inject_into_environment(self, s):
+        injection_index = self._sink_environment.index('alert()')
+        self._sink_environment = self._sink_environment[:injection_index] + s + self._sink_environment[injection_index:]
+
+        self._payloads_environment.append(s)
+
+        self.browser.get("data:text/html," + self._sink_environment)
+
     def step(self, a):
-        result = self.actions[a].run(self.url)
-        for k, v in result.items():
-            self.datastore.set(k, v)
+        result = self.actions[a].run()
+        self._inject_into_environment(result)
+
         t = self.isTerminal()
         r = self.GOAL_REWARD if t else self.STEP_REWARD
         self.datastore.save()
@@ -71,23 +191,25 @@ class HackDomain(Domain):
         return
 
 
-target_dict = {
-    "cms": 0,
-    "cms_version": 0,
-}
+state_dict = {'xpath': 0, 'control_chars': 0}
+for t in hack_actions.TAGS:  # Used to give relative numbering using xpath
+    state_dict[t] = 0
+for c in hack_actions.CONTROL_CHARS:  # Used to denote which characters are encoded
+    state_dict[c] = 0
+
 
 class Datastore(object):
     def __init__(self, f='data.json'):
         self.f = f
-        self.current_target = None
+        self.current_sink = Nones
         if os.path.exists(self.f):
             with open(self.f, 'r') as fp:
                 self.data = json.load(fp)
         else:
-            self.data = {"targets":{}}
-        self.ordered_dim_names = target_dict.keys()
+            self.data = {"sinks":{}}
+        self.ordered_dim_names = state_dict.keys()
         self.ordered_dim_names.sort()
-        self.target_urls = ['https://www.drupal.org', 'http://typographica.org', 'http://www.mb-photography.com']
+        self.all_sinks = ['<div anything="alert()"></div>']
 
     def save(self):
         with open(self.f, 'w') as fp:
@@ -116,32 +238,32 @@ class Datastore(object):
             value = ''
         return(value)
 
-    def reset(self, target_url):
-        temp_dict = dict(target_dict)
-        self.data["targets"][target_url] = dict(temp_dict)
+    def reset(self, sink):
+        temp_dict = dict(state_dict)
+        self.data["sinks"][sink] = dict(temp_dict)
 
-    def get(self, prop_name, target=None):
-        target = target if target else self.current_target
-        return(self.data["targets"][target][prop_name])
+    def get(self, prop_name, sink=None):
+        sink = sink if sink else self.current_sink
+        return(self.data["sinks"][sink][prop_name])
 
-    def set(self, prop_name, prop_value, target=None):
-        target = target if target else self.current_target
+    def set(self, prop_name, prop_value, sink=None):
+        sink = sink if sink else self.current_sink
         try:
-            targets = self.data["targets"][target]
+            targets = self.data["sinks"][sink]
         except KeyError:
-            self.data["targets"][target] = dict(target_dict)
+            self.data["sinks"][sink] = dict(state_dict)
         finally:
-            self.data["targets"][target][prop_name] = self._get_prop_numbered_value(prop_name, prop_value)
+            self.data["sinks"][sink][prop_name] = self._get_prop_numbered_value(prop_name, prop_value)
 
     def get_state(self, new=False):
         if new == True:
-            self.current_target = random.choice(self.target_urls)
-            self.reset(self.current_target)
-        state = [self.data["targets"][self.current_target][x] for x in self.ordered_dim_names]
+            self.current_sink = random.choice(self.all_sinks)
+            self.reset(self.current_sink)
+        state = [self.data["sinks"][self.current_sink][x] for x in self.ordered_dim_names]
         return(state)
 
     def get_verbose_state(self):
         s = ''
         for x in self.ordered_dim_names:
-            s += "%s: %s\t" % (x, str(self._get_prop_string_value(x, self.data["targets"][self.current_target][x])))
+            s += "%s: %s\t" % (x, str(self._get_prop_string_value(x, self.data["sinks"][self.current_sink][x])))
         return(s)
